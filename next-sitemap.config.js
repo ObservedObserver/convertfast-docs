cc1409c77b25b25549f084664dee1506f2a43089
observedobserver <270001151@qq.com>
2025-03-28 19:42:23 +0800

    fix: remove index sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,6 @@
 module.exports = {
     siteUrl: process.env.SITE_URL || "https://ui.convertfa.st",
     generateRobotsTxt: true, // (optional)
+    generateIndexSitemap: false
     // ...other options
 };
